86f8bc513e6220499a9a3933c47d7e1cc88650d9
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-12-05 08:00:42 +0530

    Update github_dis.py

--- github_dis.py
+++ github_dis.py
@@ -5,8 +5,15 @@ from discord import app_commands
 import SoraDBlite 
 from SoraDBlite import SoraDBlite, SoraDBLiteError
 
+m_uri = ""
+m_password = ""
+m_collection = ""
+
+db = SoraDBlite()
+
+db.connect(m_uri, m_password, m_collection)
+
 class disc(Commands.Bot):
-  
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -16,4 +23,13 @@ guild_id = discord.Object(1291413928458588200)
 
 @client.tree.command(name="add_github", description="just add your link of the github repo", guild=guild_id)
 async def addgithub(msg:discord.Interaction, link:str, channelid:str):
+  guildi_d = msg.guild.id
+    u_id = msg.user.id
+    onoff = on_off.lower()
+    channel_id = int(channel_id)
+
+    guild = client.get_guild(guildi_d)
+    chnl = guild.get_channel(channel_id)
+    user = await client.fetch_user(u_id)
+
   
